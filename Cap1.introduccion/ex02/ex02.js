ba8a3c94c2f8c72e769e19f1a16a61b9bdd34724
JafettWh <jafethernandez278@gmail.com>
2026-05-28 17:08:13 -0500

    Primeras practicas JS

---
+++ Cap1.introduccion/ex02/ex02.js
@@ -0,0 +1,30 @@
+var nombre; // Declaración de la variable nombre
+nombre = "Juan"; // Asignación de un valor de la variable "nombre"
+console.log(nombre); // Muestra el valor de la variable "nombre" en la consola
+
+var nombre="María";
+console.log(nombre);
+var nombre="Carlos";
+console.log(nombre);
+var nombre="Ana";
+console.log(nombre);
+
+// /Uso del la palabra reservada 'let' para declarar una variable llamada 'bebida' y asignarle diferentes valores a lo largo del código.
+//  Cada vez que se asigna un nuevo valor a la variable 'bebida', se muestra su contenido en la consola.Diferencia entre var y let: 
+// La palabra reservada 'var' tiene un alcance de función, lo que significa que la variable declarada con 'var' es accesible dentro 
+// de la función en la que se declara. Por otro lado, 'let' tiene un alcance de bloque, lo que significa que la variable declarada con 'let'
+//  solo es accesible dentro del bloque en el que se declara (por ejemplo, dentro de un bucle o una estructura condicional). Además, 'let' 
+// no permite redeclarar la misma variable dentro del mismo bloque, mientras que 'var' sí lo permite.
+
+let bebida; // Declaración de la plabra "Bebida"
+bebida = "Coca-cola"; // Asignación de un valor a la vaiable "Bebida"
+console.log(bebida); // Muestra el valor de la vaiable "Bebida"
+
+bebida="Sprite"; // Reasignación de un valor a la variable "Bebida"
+console.log(bebida); // Muestra el valor de la variable "Bebida" en la consola
+
+bebida="Fanta";
+console.log(bebida);
+
+const PI = 3.1416; // Declaración de la constante "PI"
+console.log(PI); 
