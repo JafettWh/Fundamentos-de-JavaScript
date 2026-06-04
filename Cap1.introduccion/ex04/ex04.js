6e61a8ec5c8858865b1b6a7a5fe3b7c52763fd6f
JafettWh <jafethernandez278@gmail.com>
2026-06-04 16:43:23 -0500

    segunda subida

---
+++ Cap1.introduccion/ex04/ex04.js
@@ -0,0 +1,7 @@
+let nombre, edad, direccion, movil, email;
+
+nombre = prompt("Ingrese su nombre: "); // Se piden los datos al usuario a través de la función prompt, que muestra un cuadro de diálogo para ingresar información. El valor ingresado se asigna a la variable correspondiente.
+edad = prompt("Ingrese su edad: ");
+direccion = prompt("Ingrese su dirección: ");
+movil = prompt("Ingrese su número de móvil: ");
+email = prompt("Ingrese su correo electrónico: ");
